a43e846b00cb3ecb5ffa87eed3200ddf25292f76
Maxence Caron-Lasne <maxence.caron-lasne@epita.fr>
2019-11-02 13:35:10 +0100

    test: add example module with associed test declaration

---
+++ tests/modules/example.py
@@ -0,0 +1,16 @@
+from migen import *
+
+class ExampleModule(Module):
+    def __init__(self, width):
+        self.i_first = Signal(width)
+        self.i_second = Signal(width)
+
+        self.o_first = Signal(width)
+        self.o_second = Signal(width)
+
+        ###
+
+        self.sync += [
+            self.o_first.eq(self.i_first),
+            self.o_second.eq(self.i_second)
+        ]
