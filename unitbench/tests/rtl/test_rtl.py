73a257fe256cb29baa7ad5abc1f73dedf4f98ff8
Maxence Caron-Lasne <maxence.caron-lasne@epita.fr>
2019-11-03 22:48:17 +0100

    refacto: factor rtl_test using env object.
    Using an environmnent object, module and attributes making is
    factorized.

--- unitbench/tests/rtl/test_rtl.py
+++ unitbench/tests/rtl/test_rtl.py
@@ -1,4 +1,5 @@
 import pytest
+import inspect
 from ...testmodule import SuperTestModule
 from ..modules.example import ExampleModule
 from migen import *
@@ -16,7 +17,8 @@ def make_test_module(testattr, dut_class, args=None, specials=None):
 def sim_test_module(mod, testattr, is_meant_to_succeed):
     yield mod.i_go.eq(1)
 
-    for i in range((testattr.ticks_after_inputs + testattr.ticks_after_outputs) * len(testattr.io_decl) + 2):
+    for i in range((testattr.ticks_after_inputs + testattr.ticks_after_outputs)
+                   * len(testattr.io_decl) + 2):
         yield
 
     is_over = yield mod.o_over
@@ -25,138 +27,120 @@ def sim_test_module(mod, testattr, is_meant_to_succeed):
     assert(is_over == 1)
     assert((is_success == 1) == is_meant_to_succeed)
 
+class EnvExample():
+    def __init__(self):
+        self.tag = ""
+        self.io_decl = []
+        self.ticks_after_inputs = 0
+        self.ticks_after_outputs = 0
+        self.is_meant_to_succeed = True
+        self.dut_class = ExampleModule
+
+    def runsim(self):
+        testattr = MockTestAttributes(
+            self.tag, self.io_decl,
+            self.ticks_after_inputs,
+            self.ticks_after_outputs)
+
+        dut = make_test_module(testattr, self.dut_class, [8])
+
+        run_simulation(
+            dut, sim_test_module(dut, testattr, self.is_meant_to_succeed),
+            vcd_name=testattr.tag + ".vcd")
+
 def test_example_positive_1():
-    def make_example_testattr():
-        tag = "example-positive-1"
-        io_decl = [
-            ({"i_first": 1, "i_second": 1}, {"o_first": 1, "o_second": 1})
-        ]
-        ticks_after_inputs = 1
-        ticks_after_outputs = 0
+    env = EnvExample()
 
-        return MockTestAttributes(tag, io_decl, ticks_after_inputs, ticks_after_outputs)
+    env.tag = "example-positive-1"
 
-    is_meant_to_succeed = True
+    env.io_decl = [
+        ({"i_first": 1, "i_second": 1}, {"o_first": 1, "o_second": 1})
+    ]
 
-    testattr = make_example_testattr()
-    tag = testattr.tag + ".vcd"
+    env.ticks_after_inputs = 1
+    env.ticks_after_outputs = 0
 
-    dut_class = ExampleModule
-    dut = make_test_module(testattr, dut_class, [8])
+    env.is_meant_to_succeed = True
 
-    run_simulation(
-        dut, sim_test_module(dut, testattr, is_meant_to_succeed), vcd_name=tag)
+    env.runsim()
 
 def test_example_negative_1():
-    def make_example_testattr():
-        tag = "example-negative-1"
-        io_decl = [
-            ({"i_first": 2, "i_second": 2}, {"o_first": 1, "o_second": 1}),
-        ]
-        ticks_after_inputs = 1
-        ticks_after_outputs = 0
+    env = EnvExample()
 
-        return MockTestAttributes(tag, io_decl, ticks_after_inputs, ticks_after_outputs)
+    env.tag = "example-negative-1"
 
-    is_meant_to_succeed = False
+    env.io_decl = [
+        ({"i_first": 2, "i_second": 2}, {"o_first": 1, "o_second": 1}),
+    ]
 
-    testattr = make_example_testattr()
-    tag = testattr.tag + ".vcd"
+    env.ticks_after_inputs = 1
+    env.ticks_after_outputs = 0
 
-    dut_class = ExampleModule
-    dut = make_test_module(testattr, dut_class, [8])
+    env.is_meant_to_succeed = False
 
-    run_simulation(
-        dut, sim_test_module(dut, testattr, is_meant_to_succeed), vcd_name=tag)
+    env.runsim()
 
 def test_example_multi_positive_1():
-    def make_example_testattr():
-        tag = "example-multi-positive-1"
-        io_decl = [
-            ({"i_first": 1, "i_second": 1}, {"o_first": 1, "o_second": 1}),
-            ({"i_first": 2, "i_second": 2}, {"o_first": 2, "o_second": 2})
-        ]
-        ticks_after_inputs = 1
-        ticks_after_outputs = 0
+    env = EnvExample()
 
-        return MockTestAttributes(tag, io_decl, ticks_after_inputs, ticks_after_outputs)
+    env.tag = "example-multi-positive-1"
 
-    is_meant_to_succeed = True
+    env.io_decl = [
+        ({"i_first": 1, "i_second": 1}, {"o_first": 1, "o_second": 1}),
+        ({"i_first": 2, "i_second": 2}, {"o_first": 2, "o_second": 2})
+    ]
 
-    testattr = make_example_testattr()
-    tag = testattr.tag + ".vcd"
+    env.ticks_after_inputs = 1
+    env.ticks_after_outputs = 0
 
-    dut_class = ExampleModule
-    dut = make_test_module(testattr, dut_class, [8])
+    env.is_meant_to_succeed = True
 
-    run_simulation(
-        dut, sim_test_module(dut, testattr, is_meant_to_succeed), vcd_name=tag)
+    env.runsim()
 
 def test_example_multi_negative_1():
-    def make_example_testattr():
-        tag = "example-multi-negative-1"
-        io_decl = [
-            ({"i_first": 2, "i_second": 2}, {"o_first": 1, "o_second": 1}),
-            ({"i_first": 1, "i_second": 1}, {"o_first": 2, "o_second": 2})
-        ]
-        ticks_after_inputs = 1
-        ticks_after_outputs = 0
+    env = EnvExample()
 
-        return MockTestAttributes(tag, io_decl, ticks_after_inputs, ticks_after_outputs)
+    env.tag = "example-multi-negative-1"
 
-    is_meant_to_succeed = False
+    env.io_decl = [
+        ({"i_first": 2, "i_second": 2}, {"o_first": 1, "o_second": 1}),
+        ({"i_first": 1, "i_second": 1}, {"o_first": 2, "o_second": 2})
+    ]
 
-    testattr = make_example_testattr()
-    tag = testattr.tag + ".vcd"
+    env.ticks_after_inputs = 1
+    env.ticks_after_outputs = 0
 
-    dut_class = ExampleModule
-    dut = make_test_module(testattr, dut_class, [8])
+    env.is_meant_to_succeed = False
 
-    run_simulation(
-        dut, sim_test_module(dut, testattr, is_meant_to_succeed), vcd_name=tag)
+    env.runsim()
 
 def test_example_multi_diff_1():
-    def make_example_testattr():
-        tag = "example-multi-diff-1"
-        io_decl = [
-            ({"i_first": 1, "i_second": 1}, {"o_first": 2, "o_second": 2}),
-            ({"i_first": 3, "i_second": 3}, {"o_first": 3, "o_second": 3})
-        ]
-        ticks_after_inputs = 1
-        ticks_after_outputs = 0
+    env = EnvExample()
 
-        return MockTestAttributes(tag, io_decl, ticks_after_inputs, ticks_after_outputs)
+    env.tag = "example-multi-diff-1"
 
-    is_meant_to_succeed = False
+    env.io_decl = [
+        ({"i_first": 1, "i_second": 1}, {"o_first": 2, "o_second": 2}),
+        ({"i_first": 3, "i_second": 3}, {"o_first": 3, "o_second": 3})
+    ]
 
-    testattr = make_example_testattr()
-    tag = testattr.tag + ".vcd"
+    env.ticks_after_inputs = 1
+    env.ticks_after_outputs = 0
 
-    dut_class = ExampleModule
-    dut = make_test_module(testattr, dut_class, [8])
+    env.is_meant_to_succeed = False
 
-    run_simulation(
-        dut, sim_test_module(dut, testattr, is_meant_to_succeed), vcd_name=tag)
+    env.runsim()
 
 def test_example_multi_diff_2():
-    def make_example_testattr():
-        tag = "example-multi-diff-2"
-        io_decl = [
-            ({"i_first": 1, "i_second": 1}, {"o_first": 1, "o_second": 1}),
-            ({"i_first": 2, "i_second": 2}, {"o_first": 3, "o_second": 3})
-        ]
-        ticks_after_inputs = 1
-        ticks_after_outputs = 0
+    env = EnvExample()
+    env.tag = "example-multi-diff-2"
+    env.io_decl = [
+        ({"i_first": 1, "i_second": 1}, {"o_first": 1, "o_second": 1}),
+        ({"i_first": 2, "i_second": 2}, {"o_first": 3, "o_second": 3})
+    ]
+    env.ticks_after_inputs = 1
+    env.ticks_after_outputs = 0
 
-        return MockTestAttributes(tag, io_decl, ticks_after_inputs, ticks_after_outputs)
+    env.is_meant_to_succeed = False
 
-    is_meant_to_succeed = False
-
-    testattr = make_example_testattr()
-    tag = testattr.tag + ".vcd"
-
-    dut_class = ExampleModule
-    dut = make_test_module(testattr, dut_class, [8])
-
-    run_simulation(
-        dut, sim_test_module(dut, testattr, is_meant_to_succeed), vcd_name=tag)
+    env.runsim()
