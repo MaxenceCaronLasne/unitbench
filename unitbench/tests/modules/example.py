8845c646a4a52ee0e01fdeb6e68ab0cb2160766c
Maxence Caron-Lasne <maxence.caron-lasne@epita.fr>
2020-01-16 14:18:57 +0100

    tests: add BadExampleModule

--- unitbench/tests/modules/example.py
+++ unitbench/tests/modules/example.py
@@ -1,4 +1,5 @@
-from migen import *
+from migen import Module, Signal
+
 
 class ExampleModule(Module):
     def __init__(self, width):
@@ -13,4 +14,20 @@ class ExampleModule(Module):
         self.sync += [
             self.o_first.eq(self.i_first),
             self.o_second.eq(self.i_second)
+        ]
+
+
+class BadExampleModule(Module):
+    def __init__(self, width):
+        self.i_first = Signal(width)
+        self.i_second = Signal(width)
+
+        self.o_first = Signal(width)
+        self.o_second = Signal(width)
+
+        ###
+
+        self.sync += [
+            self.o_first.eq(~self.i_first),
+            self.o_second.eq(~self.i_second)
         ]
